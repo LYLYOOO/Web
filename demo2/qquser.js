a951a5cb90fcb415989c4a4c5a86f9d9f14327e8
LYLYOOO <157880370+LYLYOOO@users.noreply.github.com>
2024-02-02 17:27:27 +0800

    Add files via upload

---
+++ demo2/qquser.js
@@ -0,0 +1,13 @@
+let QQUser = {
+    name:"QQ",
+    age:30,
+    hobby:"sleep",
+    birthday:"2000 01 01",
+    number:"1234567810",
+    school:"cqupt",
+    group:"null",
+    game:"op",
+    constellation:"Pisces",
+    qq_age:"12",
+    qq_vip:"null",
+    }
